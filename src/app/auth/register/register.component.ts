fcad8af7244d34c379e2995538738648d19b4c78
roshanarah <roshanonline8@gmail.com>
2021-03-13 22:41:37 +1100

    Changed validation in login & register form

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 // import {NgbActiveModal,NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import { Router } from '@angular/router';
 import { AuthService } from '../../shared/auth.service';
+import { NgForm } from '@angular/forms';
+import { faEnvelope, faKey } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
     selector: 'app-register',
@@ -10,6 +12,9 @@ import { AuthService } from '../../shared/auth.service';
 })
 export class RegisterComponent implements OnInit{
 
+    faEnvelope = faEnvelope;
+    faKey = faKey;
+
     constructor( private authService:AuthService){}
 
     ngOnInit(){
@@ -17,7 +22,7 @@ export class RegisterComponent implements OnInit{
 
     }
 
-    register(email:string,password:string){
-         this.authService.register(email,password)
+    register(form:NgForm){
+         this.authService.register(form.value.email,form.value.password)
     }
 }
